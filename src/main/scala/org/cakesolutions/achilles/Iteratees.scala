f07769d693faeace5cf7f26fd6bfeb5da795f933
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2013-06-18 15:08:50 +0100

    More iteratees, enumeratees and goodness

--- src/main/scala/org/cakesolutions/achilles/Iteratees.scala
+++ src/main/scala/org/cakesolutions/achilles/Iteratees.scala
@@ -5,12 +5,13 @@ import com.datastax.driver.core._
 import com.datastax.driver.core.querybuilder._
 import scalaz._
 import Scalaz._
-import iteratee.{ Iteratee => I, _ }
+import iteratee._
+import Iteratee._
+import language.implicitConversions
+import scala.collection.JavaConversions._
 import scala.language.higherKinds
 
 trait CassandraImplicits {
-  import language.implicitConversions
-  import scala.collection.JavaConversions._
 
   implicit def asScalaStream(iter: java.util.Iterator[Row]): Stream[Row] = {
     asScalaIterator(iter).toStream
@@ -19,13 +20,67 @@ trait CassandraImplicits {
   implicit def asScalaStreamRS(rs: ResultSet): Stream[Row] = {
     asScalaIterator(rs.iterator()).toStream
   }
+
 }
 
-trait CassandraIteratees extends CassandraImplicits {
+trait CassandraPipes extends CassandraImplicits
+  with CassandraIteratees
+  with CassandraEnumeratees
+  with CassandraEnumerators
+
+/* Generally speaking this trait focus more on the ease of use than
+ * on "purity". In theory we should enumerate directly an Iterator without
+ * converting it into a stream, but doing so would force our entire code to
+ * live in the IO monad. using "enumerate" allow us to run pure iteratees
+ * in the Id.
+ */
+trait CassandraIteratees {
 
   //Implicitly call "collect" under the hood,
   //collecting value trasformed by @f.
   def gather[A,B](f: A => B): IterateeT[A, Id, List[B]] = {
-    I.collect[B, List] %= I.map(f)
+    collect[B, List] %= map(f)
+  }
+
+  //An iteratee which consume its input and finally dispose the
+  //generated session. Return the number of executed queries.
+  def withSession(implicit session: Session): Iteratee[Query, Int] = {
+     def step(acc: Int)(s: Input[Query]): Iteratee[Query, Int] =
+       s(el = e => {
+           session.execute(e)
+           cont(step(acc + 1))},
+         empty = cont(step(acc)),
+         eof = {
+          session.shutdown()
+          done(acc, eofInput[Query]) }
+       )
+     cont(step(0))
+  }
+
+}
+
+trait CassandraEnumeratees {
+  
+  //EnumerateeT which converts a list of String into a list of Query.
+  def toQuery: EnumerateeT[String, Query, Id] = {
+    map((rq:String) => new SimpleStatement(rq))
+  }
+
+}
+
+trait CassandraEnumerators {
+
+  //Handle with care. Enumerating a ResultSetFuture will cause
+  //the future to block and wait for the result.
+  def enumerateRSF(rsf: ResultSetFuture): Enumerator[Row] = {
+    enumerate(asScalaIterator(rsf.get().iterator()).toStream)
+  }
+
+  def enumQueries(queries: List[Query]): EnumeratorT[Query, Id] = {
+    enumList(queries)
+  }
+
+  def enumRawQueries(queries: List[String]): EnumeratorT[String, Id] = {
+    enumList(queries)
   }
 }
--- src/test/scala/org/cakesolutions/achilles/IterateesSpec.scala
+++ src/test/scala/org/cakesolutions/achilles/IterateesSpec.scala
@@ -1,7 +1,7 @@
 
 package org.cakesolutions.achilles
 
-import org.specs2.mutable.{ Specification, After }
+import org.specs2.mutable.{ Specification, Before }
 import org.specs2.specification.{ Step, Fragments }
 import com.datastax.driver.core._
 import com.datastax.driver.core.querybuilder._
@@ -10,46 +10,50 @@ import scalaz._
 import Scalaz._
 import iteratee.{ Iteratee => I }
 
-class IterateesSpec extends Specification with CassandraIteratees {
+class IterateesSpec extends Specification with CassandraPipes {
 
   override def map(fs: =>Fragments) = Step(createDb) ^ fs ^ Step(dropDb)
+  val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
 
   def createDb = {
-    val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
-    val session = cluster.connect()
+    implicit val session = cluster.connect()
     val prevKeyspaces = cluster.getMetadata().getKeyspaces().size
-    val query = """CREATE KEYSPACE simplex WITH replication 
-                = {'class':'SimpleStrategy', 'replication_factor':3};"""
-    session.execute(query)
+    var queries = List(
+      """CREATE KEYSPACE simplex WITH replication 
+                = {'class':'SimpleStrategy', 'replication_factor':3};""",
 
-    session.execute("""CREATE TABLE simplex.songs ( id uuid PRIMARY KEY,
-                       title text, album text, artist text,
-                       tags set<text>, data blob);""")
-    session.execute("""INSERT INTO simplex.songs (id, title, album, artist, tags)
-                       VALUES ( 756716f7-2e54-4715-9f00-91dcbea6cf50,
-                                'La Petite Tonkinoise', 'Bye Bye Blackbird',
-                                'Joséphine Baker', {'jazz', '2013'})""")
-    cluster.shutdown()
+      """CREATE TABLE simplex.songs ( id uuid PRIMARY KEY,
+                title text, album text, artist text,
+                tags set<text>, data blob);""",
+
+      """INSERT INTO simplex.songs (id, title, album, artist, tags)
+                VALUES ( 756716f7-2e54-4715-9f00-91dcbea6cf50,
+                         'La Petite Tonkinoise', 'Bye Bye Blackbird',
+                         'Joséphine Baker', {'jazz', '2013'})""",
+
+      """CREATE TABLE simplex.playlist ( id uuid PRIMARY KEY,
+                       title text, ordering int);""")
+
+    (1 to 10) map { order =>
+      val uuid = UUID.randomUUID().toString
+      queries = queries :+ ("""INSERT INTO simplex.playlist (id, title, ordering)
+                            VALUES ( %s, 'What a Boring Playlist', %s)""" format(uuid, order))
+    }
+
+    (withSession %= toQuery &= enumRawQueries(queries)).run
   }
 
   def dropDb = {
-    val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
     val session = cluster.connect()
     session.execute("DROP KEYSPACE simplex;")
     cluster.shutdown()
-  }
-
-  trait ClusterLifeCycle extends After {
-    val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
-
-    def after = cluster.shutdown()
   }
 
   sequential
 
   "Cassandra Iteratees" should {
 
-    "enumerate underlying iterators" in new ClusterLifeCycle {
+    "enumerate underlying iterators" in {
       val session = cluster.connect()
       val clause = QueryBuilder.eq("id", UUID.fromString("756716f7-2e54-4715-9f00-91dcbea6cf50"))
       val query = QueryBuilder.select.all.from("simplex", "songs").where(clause)
@@ -58,7 +62,7 @@ class IterateesSpec extends Specification with CassandraIteratees {
       size mustEqual(1)
     }
 
-    "collects all songs name with appropriate iteratee" in new ClusterLifeCycle {
+    "collects all songs name with appropriate iteratee" in {
 
       def getTitle(r: Row): String = r.getString("title")
 
@@ -71,12 +75,20 @@ class IterateesSpec extends Specification with CassandraIteratees {
       titles.head mustEqual("La Petite Tonkinoise")
     }
 
-    "collects all songs name using \"gather\"" in new ClusterLifeCycle {
+    "collects all songs name using \"gather\"" in {
       val session = cluster.connect()
       val query = QueryBuilder.select.all.from("simplex", "songs")
       val results = session.execute(query)
       val titles = (gather((r:Row) => r.getString("title")) &= I.enumerate(results)).run
       titles.head mustEqual("La Petite Tonkinoise")
+    }
+
+    "allow async IO" in {
+      val session = cluster.connect()
+      val query2 = QueryBuilder.select.all.from("simplex", "playlist")
+      val res2 = session.executeAsync(query2)
+      val orders = (gather((r:Row) => r.getInt("ordering")) &= enumerateRSF(res2)).run
+      orders.sum mustEqual(55)
     }
 
   }
